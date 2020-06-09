e96ea1199890e98d59f156cf4fbeeb5a4f35e4fb
caperso <qq174028@sina.com>
2020-06-09 10:54:20 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,10 @@
 "use strict";
+// const ENV = process.env.NODE_ENV;
+// const DEPS =
+//   ENV !== "production" && ENV !== "test" && typeof console !== undefined;
+// /*  */
+// if (DEPS) {
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
 var lowpare_1 = require("./lowpare");
 exports.lowSame = lowpare_1.lowSame;
